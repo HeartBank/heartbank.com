41a9f469f92976c9bc6f2504de6d8dd5f1dc9f59
Thon Ly <thonly@heartbank.ceo>
2022-10-13 17:43:16 -0700

    begin v2

---
+++ index.js
@@ -0,0 +1,16 @@
+import "./components/sw-header/element.mjs";
+
+import "./components/sw-main/sw-curriculum/element.mjs";
+import "./components/sw-main/sw-podcast/element.mjs";
+import "./components/sw-main/sw-quiz/element.mjs";
+import "./components/sw-main/sw-code/element.mjs";
+import "./components/sw-main/sw-cohort/element.mjs";
+import "./components/sw-main/sw-coupon/element.mjs";
+import "./components/sw-main/element.mjs";
+
+import "./components/sw-footer/element.mjs";
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-N5YQNCN990');
