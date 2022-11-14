204d78722f5a51a1443c3da79c6bc7adc7dafd47
Thon Ly <thonly@heartbank.ceo>
2022-11-14 08:15:12 -0800

    rename

--- components/hb-main/template.mjs
+++ components/hb-main/template.mjs
@@ -3,9 +3,9 @@ const template = document.createElement("template");
 // Reference: https://codepen.io/team/css-tricks/pen/GRggVBg
 
 template.innerHTML = `
-    <link rel="stylesheet" href="components/sw-main/shadow.css">
+    <link rel="stylesheet" href="components/hb-main/shadow.css">
     <slot></slot>
-    <img src="components/sw-main/background.svg">
+    <img src="components/hb-main/background.svg">
 `;
 
 export default template;
